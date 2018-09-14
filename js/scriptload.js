6b5caad9dbbd474b94dfa051b0452abf72464a09
Samc <serg1oamc@hotmail.com>
2018-09-14 15:47:34 -0500

    Update scriptload.js

--- js/scriptload.js
+++ js/scriptload.js
@@ -6,7 +6,7 @@
 	function frame(){
 		if (loading == 250){
 			clearInterval(id);
-			window.open("portfolio.html", "_self");
+			window.open("portfolio", "_self");
 		} else {
 			loading = loading + 1;
 			if(loading == 248) {
